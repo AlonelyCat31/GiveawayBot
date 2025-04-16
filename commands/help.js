b7691de1a048b91c7eef2b33e4cbc95e74344722
AlonelyCat31 <reaperx312007@gmail.com>
2025-04-16 19:59:45 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,10 +11,11 @@ module.exports = {
           "`!addcode <GameName> <Code>` - Add a new game code (authorized roles only)",
           "`!list` - List available games (authorized roles only)",
           "`!drop <GameName>` - Drop a code for claiming (authorized roles only)",
+          "`!host <GameName> <Code> <Platform>` - Host a new giveaway with a game code",
           "`!setuprole <RoleName>` - Authorize a role to use code management commands (admin only)",
-          "`!setlogchannel <#channel>` - Set log channel for claimed codes (admin only)",
-          "`!timer <hours> <minutes> <seconds>` - Set how long after a code is claimed it should be posted (authorized roles only)",
-          "`!host <GameName> <Code> <Platform>` - Let users host their own giveaways with code and platform details",
+          "`!setlogchannel` - Set current channel as the log channel (admin only)",
+          "Claimed codes will be posted in the log channel."
+          "`!timer <hours> <minutes> <seconds>` - Set a timer to drop claimed codes after a set time",
           "`!help` - Show this help message",
         ].join("\n")
       );
