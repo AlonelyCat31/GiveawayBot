2692a897564691ffd5c750ef1bf5476bcb0f0245
AlonelyCat31 <reaperx312007@gmail.com>
2025-04-16 17:55:14 +0530

    Update setlogchannel.js

--- commands/setlogchannel.js
+++ commands/setlogchannel.js
@@ -3,14 +3,22 @@ const path = require("path");
 
 module.exports = {
   name: "setlogchannel",
-  execute(message) {
+  execute(message, args) {
     if (!message.member.permissions.has("Administrator")) {
       return message.reply("Only administrators can use this command.");
     }
 
-    const configPath = path.join(__dirname, "../logchannel.json");
-    fs.writeFileSync(configPath, JSON.stringify({ channelId: message.channel.id }, null, 2));
+    // Check if a channel ID or mention is provided
+    const channelId = args[0]?.replace(/[<@!>]/g, ""); // Remove <#>, @, or ! from mention
+    const channel = message.guild.channels.cache.get(channelId) || message.mentions.channels.first();
 
-    message.reply(`✅ Log channel has been set to: ${message.channel}`);
+    if (!channel) {
+      return message.reply("Please specify a valid channel by mentioning it or providing the channel ID.");
+    }
+
+    const configPath = path.join(__dirname, "../logchannel.json");
+    fs.writeFileSync(configPath, JSON.stringify({ channelId: channel.id }, null, 2));
+
+    message.reply(`✅ Log channel has been set to: ${channel}`);
   },
 };
