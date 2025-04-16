e507f85daa2df10af24a73121d11db1340535849
AlonelyCat31 <reaperx312007@gmail.com>
2025-04-16 20:14:20 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,15 +7,12 @@ module.exports = {
       .setTitle("🎯 Giveaway Bot Commands")
       .setColor("Purple")
       .setDescription(
-        [
-          "`!addcode <GameName> <Code>` - Add a new game code (authorized roles only)",
-          "`!list` - List available games (authorized roles only)",
-          "`!drop <GameName>` - Drop a code for claiming (authorized roles only)",
+        [ "`!timer <hours> <minutes> <seconds>` - Set a timer to drop claimed codes after a set time",
           "`!host <GameName> <Code> <Platform>` - Host a new giveaway with a game code",
           "`!setuprole <RoleName>` - Authorize a role to use code management commands (admin only)",
           "`!setlogchannel` <channel> - Set current channel as the log channel (admin only)",
           "Claimed codes will be posted in the log channel."
-          "`!timer <hours> <minutes> <seconds>` - Set a timer to drop claimed codes after a set time",
+        
           "`!help` - Show this help message",
         ].join("\n")
       );
