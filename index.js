d526664465fad4e585f05a303635377b52b9bbb8
AlonelyCat31 <reaperx312007@gmail.com>
2025-04-16 12:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -135,11 +135,5 @@ client.on(Events.InteractionCreate, async (interaction) => {
       ],
       components: [],
     });
-  } catch (err) {
-    console.error("Failed to DM the user:", err);
-    interaction.reply({
-      content: "Failed to DM you the code. Do you have DMs disabled?",
-      ephemeral: true,
-    });
-  }
+  } 
 });
