a1009b9ae5bed1ea2f2b46977e6a3822b76d297f
AlonelyCat31 <reaperx312007@gmail.com>
2025-04-17 12:29:54 +0530

    Create setlogchannel.js

---
+++ commands/setlogchannel.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  name: "setlogchannel",
+  async execute(message, args) {
+    if (!message.member.permissions.has("Administrator")) {
+      return message.reply("❌ You need to be an administrator to use this command.");
+    }
+
+    const mentionedChannel = message.mentions.channels.first();
+    const channelToSet = mentionedChannel || message.channel;
+
+    const logPath = path.join(__dirname, "../logChannel.json");
+    const data = { channelId: channelToSet.id };
+
+    fs.writeFileSync(logPath, JSON.stringify(data, null, 2));
+    return message.reply(`✅ Log channel set to <#${channelToSet.id}>`);
+  },
+};
